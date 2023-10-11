9443b3613abdc52b38d3ee63b71608aff51f9ef2
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-10-11 14:56:02 +0000

    added and required gemfile

---
+++ howdy.rb
@@ -0,0 +1,13 @@
+# /howdy.rb
+
+require "active_support/all"
+require "awesome_print/all"
+require "pry-byebug/all"
+
+my_string = "Hello, world!"
+
+pp my_string
+require "./goodbye.rb"
+
+
+pp 1.ordinalize
