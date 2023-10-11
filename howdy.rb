cc6d70867da39480dbb99e24ff645b082ccaa1db
fernaldeats <42142633+fernaldeats@users.noreply.github.com>
2023-10-11 15:07:49 +0000

    initial ruby set-up

--- howdy.rb
+++ howdy.rb
@@ -1,13 +1,16 @@
 # /howdy.rb
 
 require "active_support/all"
-require "awesome_print/all"
-require "pry-byebug/all"
 
 my_string = "Hello, world!"
 
 pp my_string
 require "./goodbye.rb"
 
-
 pp 1.ordinalize
+
+pp "What's your name?"
+
+their_name = gets.chomp
+
+pp "Hello, #{their_name}!"
